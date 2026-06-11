8a81f42f2fc28f994b9a469d1ee4a85a405ff4e6
thanosgiap <thanosgiap@gmail.com>
2026-06-11 20:48:28 +0300

    Add fleet listing page and admin config

--- fleet/admin.py
+++ fleet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category",
+        "transmission",
+        "seats",
+        "price_per_day",
+        "is_available",
+    )
+    list_filter = ("category", "transmission", "is_available")
+    search_fields = ("name",)
---
+++ fleet/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "fleet"
+
+urlpatterns = [
+    path("", views.car_list, name="car_list"),
+]
